435304924ee59cd35a69aab0bc57baf5a9d0fbd1
Mason Tian <mtian@princeton.edu>
2018-11-11 05:11:04 -0500

    more tool functions

--- scripts/tools.py
+++ scripts/tools.py
@@ -3,7 +3,8 @@
 # Description: General helper functions.
 
 import os
-
+import re
+from itertools import cycle, islice
 
 # Insert a string into the beginning of originalFile
 def insert(originalfile,string):
@@ -14,7 +15,17 @@ def insert(originalfile,string):
     os.rename('newfile.csv',originalfile)
 
 
+# Return an integer of the numeric characters in string
+def return_numeric(inString):
+    return int("".join(re.findall(r"[0-9]+", inString)))
+
+
 # Returns all file names with term in the name in the local directory
 def get_data_files(term, path = "."):
     return [f for f in os.listdir(path) if term in f]
+
+
+# continously iterates over elements for length number of things
+def repeat_elements(elements, length):
+    return islice(cycle(elements), length)
 

dbd76f99cfecf8ac8881dabd24343fbca0b75d43
Mason Tian <mtian@princeton.edu>
2018-11-10 22:45:45 -0500

    initial update

---
+++ scripts/merge.py
@@ -0,0 +1,69 @@
+# Filename: merge.py
+# Author: Mason Tian
+# Description: Merges a list of individual stock list files and outputs
+# formatted excel spreadsheets
+
+
+import os 
+import pandas as pd
+import csv
+import math
+
+# script params
+
+# list of factors to call
+factors = []
+
+# MM/DD/YYYY date to start
+startdate = ""
+
+# frequency
+frequency = ""
+frequency = "per=" + frequency
+
+# function to call
+function = ""
+
+def insert(originalfile,string):
+    with open(originalfile,'r') as f:
+        with open('newfile.csv','w') as f2: 
+            f2.write(string)
+            f2.write(f.read())
+    os.rename('newfile.csv',originalfile)
+
+if __name__ == "__main__":
+    files = [f for f in os.listdir() if "US" in f]
+    print(files)
+
+    tickers = set()
+
+    for f in files:
+        df = pd.read_excel(open(f, 'rb'))
+        tickers = set(df["Ticker"]).union(tickers)
+   
+    tickers = list(tickers)
+    df_all = pd.DataFrame({'Ticker': tickers})
+    df_all.to_excel("./divided/US_Equity.xls", header=False, \
+            index=False)
+
+    factors = []
+    for factor in factors:
+        flist = []
+        for stock in tickers:
+
+            function = "=" + function + "(\"" + stock + "\",\"" \
+                    + factor + "\",\"" + startdate + "\",\"\",\"" \
+                    + frequency + "\")"
+
+            flist.append(function)
+            flist.append(" ")
+        df = pd.DataFrame({'vals':flist})
+        i = 0
+        while i < math.ceil(df.transpose().shape[1]/4000):
+            filename = \
+                    "./divided/US_Equity(" + factor + str(i) + ").csv"
+            df.transpose().iloc[:,i*4000:(i+1)*4000].to_csv(filename,\
+                    sep="^", header = False, index = False)
+            insert(filename, "sep=^\n")
+            i += 1
+
